8e1c33fc78b24cd736388ebd3b36dbeb3ebe1607
lichen404 <f360family@gmail.com>
2024-05-05 18:25:09 +0800

    修复跨域问题

--- core/config.py
+++ core/config.py
@@ -25,7 +25,7 @@ class Settings(BaseSettings):
         "http://localhost",
         "http://localhost:8000",
         "http://127.0.0.1:8000",
-        "*"
+        "http://mangosteen.lichen404.top"
     ]
 
     class Config:
